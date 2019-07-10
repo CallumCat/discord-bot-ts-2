da9acb7850d3b3b9d264de91081402540eb4738a
Ethan C <cavesystems11@gmail.com>
2019-07-10 00:18:03 -0700

    Wrap db connection around a Promise to make sure it loads before moving on

--- src/utils/DbClientManager.ts
+++ src/utils/DbClientManager.ts
@@ -3,8 +3,15 @@ import {config} from "../config/config";
 
 export class DbClientManager {
     static async connect(): Promise<Db> {
-        return await MongoClient.connect('mongodb://' + config.db.user + ':' + config.db.pass + '@' + config.db.host + ':' + config.db.port + '/' + config.db.dbName, {useNewUrlParser: true}).then(async(c) => {
-            return c.db();
+        let db: Db;
+        await new Promise(async(res) => {
+            await MongoClient.connect('mongodb://' + config.db.user + ':' + config.db.pass + '@' + config.db.host + ':' + config.db.port + '/' + config.db.dbName, {useNewUrlParser: true}).then(async(c) => {
+                db = c.db();
+            });
+            res();
+        }).catch((e) => {
+            throw e;
         });
+        return db;
     }
 }
